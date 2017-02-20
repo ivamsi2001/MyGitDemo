f302ec6538edd8b8695cb03e226ca4d1a5110663
Vamsi Krishna Aitha <ivamsi2001@gmail.com>
2017-02-20 08:35:59 +0530

    Commit 3

--- test-java/src/main/java/MyTest.java
+++ test-java/src/main/java/MyTest.java
@@ -5,6 +5,14 @@ public class MyTest {
 	}
 
 	public void m1() {
-		System.out.println("M!!!!!!!");
+		System.out.println("M1!!!!!!!");
 	}
-}
+	
+	public void m2(){
+		System.out.println("M2!!!!!!!");
+	}
+	
+	public void m5(){
+		System.out.println("M5!!!!!!!");
+	}
+}	
